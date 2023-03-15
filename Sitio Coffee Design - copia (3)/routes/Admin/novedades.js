1ff4b1458218457987e446f3d8ffe9626c0633ef
Santiago Laya <117774297+SantiagoLaya@users.noreply.github.com>
2023-03-15 01:27:26 -0300

    Sitio Coffee Design - copia (4)
    M5U1

--- Sitio Coffee Design - copia (3)/routes/Admin/novedades.js
+++ Sitio Coffee Design - copia (3)/routes/Admin/novedades.js
@@ -1,11 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
+var novedadesModel = require('../../models/novedadesModel');
+
+router.get('/', async function(req, res, next) {
+
+  var novedades = await novedadesModel.getNovedades();
+
   res.render('admin/novedades',{
   		layout:'admin/layout',
-  		persona: req.session.nombre
+  		usuario: req.session.nombre,
+  		novedades
   });
 });
+
+//para eliminar una novedad 
+router.get('/eliminar/:id', async (req, res, next) => {
+	const id = req.params.id;
+	await novedadesModel.deleteNovedadesById(id);
+	res.redirect('/admin/novedades')
+
+}); //cierre de eliminar
+
+
 
 module.exports = router;
